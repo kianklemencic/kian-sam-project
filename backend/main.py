875ebfeb1afe9609c44ec520542e855049e1672d
Samuel Belton <sambergeronbelton@gmail.com>
2026-09-04 16:16:01 -0600

    fastapi status message

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "backend is alive"}
+
+@app.get("/api/message")
+def get_message():
+    return {"message": "fastapi is alive"}
